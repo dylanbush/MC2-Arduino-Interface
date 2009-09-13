d891669e67a2281257d324a29c9f9c21a96a617c
dylanbush <dylanb20@gmail.com>
2009-09-12 22:56:17 -0400

    Fixed readme and changed Serial Lib

--- MC2Interface.cpp
+++ MC2Interface.cpp
@@ -1,16 +1,26 @@
-/************************************************************************
- *																		*
- *	MC2Interface.cpp - Library for interacting with a MC2 Motor Control	*
- *					 Interface											*
- *																		*
- *	Created by Dylan Bush, September 2009								*
- *																		*
- *																		*
- ************************************************************************/
+/************************************************************************************
+ *	MC2Interface.cpp - A library for controlling an Centroid Motion controller		*
+ *	Copyright (c) 2009 Dylan Bush 													*
+ *																					*
+ *	This library is free software; you can redistribute it and/or					*
+ *	modify it under the terms of the GNU Lesser General Public						*
+ *	License as published by the Free Software Foundation; either					*
+ *	version 2.1 of the License, or (at your option) any later version.				*
+ *																					*
+ *	This library is distributed in the hope that it will be useful,					*
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of					*
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU				*
+ *	Lesser General Public License for more details.									*
+ *																					*
+ *	You should have received a copy of the GNU Lesser General Public				*
+ *	License along with this library; if not, write to the Free Software				*
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA		*
+ *																					*
+ ************************************************************************************/
+
 
 #include "WProgram.h"
 #include "MC2Interface.h"
-#include "MC2Serial.h"
 
 MC2Interface::MC2Interface(int txPin, int rxPin, int motor)
 {
